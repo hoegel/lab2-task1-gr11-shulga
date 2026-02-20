f3a408c4ffe41336d2e7ca20867cf453d2ee7703
hoegel <111231050+hoegel@users.noreply.github.com>
2026-02-20 15:48:43 +0000

    Created function for conversion from narrow to wide char string

--- src/main.c
+++ src/main.c
@@ -4,17 +4,30 @@
 #include <stdio.h>
 #include <wchar.h>
 #include <locale.h>
+#include <stdlib.h>
+#include <string.h>
 #include "count_n.h"
 
-int main (int argc, char *argv[])
-{
-  if(argc != 3) {
-    printf("error: invalid arguments\nusage: <string1> <string2>\n");
-    return 0;
-  }
+wchar_t *GetWC(const char *c);
 
-  printf("first string: %s\n", argv[1]);
-  printf("second string: %s\n", argv[2]);
-  
-  return 0; 
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        printf("error: invalid arguments\nusage: <string1> <string2>\n");
+        return 0;
+    }
+
+    return 0;
+}
+
+wchar_t *GetWC(const char *c) {
+    if (c == NULL) return NULL;
+    size_t len = strlen(c) + 1;
+    wchar_t *wc = malloc(len * sizeof(wchar_t));  // alloc mem for wchar
+    if (wc == NULL) return NULL;                  // malloc failure
+    size_t converted = mbstowcs(wc, c, len);      // convert multibyte string to wchar
+    if (converted == (size_t)-1) {
+        free(wc);
+        return NULL;
+    }
+    return wc;
 }
