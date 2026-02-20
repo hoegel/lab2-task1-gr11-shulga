261146a8430c27a1ddfea98ad91ccd3d0c5a66a9
hoegel <111231050+hoegel@users.noreply.github.com>
2026-02-20 14:48:39 +0000

    Added count function

--- src/main.c
+++ src/main.c
@@ -1,8 +1,23 @@
 /* hello.c */
 /* Author: hoegel */
 #include <stdio.h>
+#include <wchar.h>
+#include <locale.h>
+
+int count_n(const wchar_t *s);
 
 void main (void)
 {
+  //setlocale(LC_ALL, "");
+  const wchar_t *str = L"привеннн, мир";
+  printf("%d\n", count_n(str));
+}
 
+int count_n(const wchar_t *s) {
+    int cnt = 0;
+    while (*s) {
+        if (*s == L'н') ++cnt;
+        ++s;
+    }
+    return cnt;
 }
